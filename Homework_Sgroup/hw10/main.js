e3d20ce234a29a33276f6ba6c8bc7c9415881a5a
Thanh Tu <thanhthanhvava2004@gmail.com>
2024-03-13 20:32:32 +0700

    hw10

---
+++ Homework_Sgroup/hw10/main.js
@@ -0,0 +1,18 @@
+const a = document.querySelector("h1");
+const b = document.querySelector("div");
+const c = document.querySelector("img");
+
+
+setTimeout(function(){
+    fetch ("https://pokeapi.co/api/v2/pokemon/pikachu")
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+            console.log(data)
+            a.innerText = data.name;
+            b.style.display = "block";
+            c.src = data.sprites.front_default
+            ;
+        })
+}, 3000)
